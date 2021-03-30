e3022aa3fcde0d9bceb84a3f5d2d9ade31855cfb
Stanislav <te100ff@inbox.ru>
2021-03-30 18:58:43 +0300

    log out complete

---
+++ LoginApp/GreetingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GreetingViewController.swift
+//  LoginApp
+//
+//  Created by Stanislav Testov on 30.03.2021.
+//
+
+import UIKit
+
+class GreetingViewController: UIViewController {
+
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    var userGreetingName: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        greetingLabel.text = userGreetingName
+
+    }
+   
+    @IBAction func logOutPressed() {
+        dismiss(animated: true)
+    }
+    
+   
+}
---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Stanislav Testov on 30.03.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var forgotNameButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+   
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    private let userName = "Stas"
+    private let password = "Testov"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let greetingVC = segue.destination as? GreetingViewController else { return }
+        greetingVC.userGreetingName = "Hello, \(userNameTextField.text ?? "hacker")!"
+    }
+    
+   
+    
+    
+    @IBAction func forgotPressed(_ sender: UIButton) {
+        switch sender {
+        case forgotNameButton:
+            showAlert(type: "name", data: userName)
+        default:
+            showAlert(type: "password", data: password)
+            passwordTextField.text = ""
+        }
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let _ = segue.source as? GreetingViewController else { return }
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    
+    private func showAlert(type: String, data: String) {
+        let alert = UIAlertController(
+            title: "Let me see...",
+            message: "Your \(type) is \(data) 🔑",
+            preferredStyle: .alert
+        )
+        
+        let alertAction = UIAlertAction(title: "Oki Doki", style: .default)
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+
+}
+
