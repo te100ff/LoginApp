d4b544e65c0b751c9f0ad024c9586de96ff2048e
Stanislav <te100ff@inbox.ru>
2021-03-30 19:39:32 +0300

    Adding image

--- LoginApp/GreetingViewController.swift
+++ LoginApp/GreetingViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 class GreetingViewController: UIViewController {
 
     @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
     
     var userGreetingName: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         greetingLabel.text = userGreetingName
-
     }
    
     @IBAction func logOutPressed() {
